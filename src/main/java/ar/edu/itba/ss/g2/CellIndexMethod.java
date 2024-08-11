438a3fa2719bbf51f7114c724cfc508f49c8ec5b
Gonzalo A. Martone <gmartone@itba.edu.ar>
2024-08-11 14:44:10 -0300

    feat: add Particle class

---
+++ src/main/java/ar/edu/itba/ss/g2/CellIndexMethod.java
@@ -0,0 +1,11 @@
+package ar.edu.itba.ss.g2;
+
+import java.util.List;
+import java.util.Map;
+
+public class CellIndexMethod {
+
+    public static Map<Particle, List<Particle>> calculate(List<Particle> particles, Double L, Long M, Double rc) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/main/java/ar/edu/itba/ss/g2/Particle.java
@@ -0,0 +1,27 @@
+package ar.edu.itba.ss.g2;
+
+public class Particle {
+
+    private final Double x;
+    private final Double y;
+
+    private final Double radius;
+
+    public Particle(Double x, Double y, Double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public Double getX() {
+        return x;
+    }
+
+    public Double getY() {
+        return y;
+    }
+
+    public Double getRadius() {
+        return radius;
+    }
+}
