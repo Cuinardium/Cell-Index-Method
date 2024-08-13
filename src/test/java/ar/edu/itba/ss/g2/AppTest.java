a48baaa44223427ff6b2b1177d43a37fedf2ba13
Santiago Ballerini <sballerini@itba.edu.ar>
2024-08-13 17:07:12 -0300

    feat: add tests

--- src/test/java/ar/edu/itba/ss/g2/AppTest.java
+++ src/test/java/ar/edu/itba/ss/g2/AppTest.java
@@ -1,38 +1,53 @@
 package ar.edu.itba.ss.g2;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import ar.edu.itba.ss.g2.state.Input;
+import ar.edu.itba.ss.g2.state.Output;
+import ar.edu.itba.ss.g2.utils.FileUtil;
+
+import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.stream.Stream;
+
+public class AppTest {
+    @TempDir static File tempDir;
+
+    public static Stream<Integer> provideM() {
+        return Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    @ParameterizedTest
+    @MethodSource("provideM")
+    public void testApp(int M) throws IOException {
+        Input input = FileUtil.deserializeInput("src/test/resources");
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+        var a = CellIndexMethod.calculate(input.getParticles(), input.getL(), (long) M, 1.0);
+
+        String expectedOutputFile = "src/test/resources/output.txt";
+
+        FileUtil.serializeOutput(new Output(a, 0), tempDir.getAbsolutePath());
+
+        File expectedOutput = new File(expectedOutputFile);
+
+        File actualOutput = new File(tempDir.getAbsolutePath() + "/output.txt");
+
+        try (BufferedReader expectedReader = new BufferedReader(new FileReader(expectedOutput));
+                BufferedReader actualReader = new BufferedReader(new FileReader(actualOutput))) {
+
+            String expectedLine;
+            String actualLine;
+
+            while ((expectedLine = expectedReader.readLine()) != null) {
+                actualLine = actualReader.readLine();
+                assertEquals(expectedLine, actualLine);
+            }
+        }
     }
 }
