ac828b161874ba7881a2f1ce0d41c2a5963a91ef
Franco Bosetti <francobosetti@live.com.ar>
2024-08-15 17:23:00 -0300

    fix: parse utils appropiate for format

--- graph_particles/parse_utils.py
+++ graph_particles/parse_utils.py
@@ -1,5 +1,9 @@
-STATIC_DYNAMIC_DELIMITER = '   '
-OUTPUT_DELIMITER = ','
+DELIMITER = ' '
+N_INDEX = 0
+L_INDEX = 1
+T_INDEX = 0
+SIMULATION_TIME_INDEX = 0
+
 def parse_static(path):
     with open(path, 'r') as f:
         lines = f.readlines()
@@ -7,13 +11,15 @@ def parse_static(path):
     static_values = []
     n, l = 0, 0
     for i, line in enumerate(lines):
-        if i == 0:
+        if i == N_INDEX:
             n = int(line)
-        elif i == 1:
+        elif i == L_INDEX:
             l = int(line)
         else:
-            line_values = line.strip().split(STATIC_DYNAMIC_DELIMITER)
-            static_values.append([float(v) for v in line_values])
+            # If there is more than one value per line, use this instead
+            # line_values = line.strip().split(DELIMITER)
+            # static_values.append(float(v) for v in line_values])
+            static_values.append(float(line))
 
     return n, l, static_values
 
@@ -24,22 +30,26 @@ def parse_dynamic(path):
     dynamic_values = []
     t = 0
     for i, line in enumerate(lines):
-        if i == 0:
+        if i == T_INDEX:
             t = int(line)
         else:
-            line_values = line.strip().split(STATIC_DYNAMIC_DELIMITER)
+            line_values = line.strip().split(DELIMITER)
             dynamic_values.append([float(v) for v in line_values])
 
     return t, dynamic_values
 
 
-def parse_neighbours(path):
+def parse_output(path):
     with open(path, 'r') as f:
         lines = f.readlines()
 
     neighbours = {}
-    for line in lines:
-        line_values = line.strip().split(OUTPUT_DELIMITER)
-        neighbours[int(line_values[0])] = [int(v) for v in line_values[1:]]
+    simulation_time = 0
+    for i, line in enumerate(lines):
+        if i == SIMULATION_TIME_INDEX:
+            simulation_time = int(line)
+        else:
+            line_values = line.strip().split(DELIMITER)
+            neighbours[int(line_values[0])] = [int(v) for v in line_values[1:]]
 
-    return neighbours
+    return simulation_time, neighbours
